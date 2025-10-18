6ce9039f9c9a84b720cd9988e39ec071a54c3792
jovan196 <19623032@std.stei.itb.ac.id>
2025-10-18 15:54:39 +0700

    feat: testcase

---
+++ test/milestone-1/input-2.pas
@@ -0,0 +1,15 @@
+program Hello2;
+var
+  a, b, i: integer;
+  arr: array[1..5] of integer;
+
+begin
+  a := 5;
+  b := a + 10;
+  writeln('Result = ', b);
+  for i := 1 to 5 do
+  begin
+    arr[i] := i + 1;
+    writeln(arr[i]);
+  end;
+end.
