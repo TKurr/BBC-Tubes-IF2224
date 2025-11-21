b41bb18128b5fd34384d4900c678098a3003a317
TKurr <theokurniady12@gmail.com>
2025-11-21 20:20:02 +0700

    test: add test cases

---
+++ test/milestone-3/input/input-3.pas
@@ -0,0 +1,16 @@
+program RangesLoops;
+variabel
+  i: integer;
+  a: larik[0 .. 3] dari integer;
+mulai
+  a[0] := 1;
+  untuk i := 0 ke 3 lakukan
+    a[i] := i;
+  untuk i := 3 turun_ke 0 lakukan
+    a[i] := a[i] + 1;
+  jika a[1] < a[2] maka
+    a[1] := a[2];
+  selama i > 0 lakukan
+    i := i - 1;
+  writeln('ok', a[1]);
+selesai.
